4232a00a4f3edc84c7e2ac6cf4ad158809c082ad
gbgraciano-hue <gbgraciano@gmail.com>
2025-08-19 01:40:24 +0000

    AULA(2)

--- src/main/java/app/Aluno.java
+++ src/main/java/app/Aluno.java
@@ -1,6 +1,20 @@
 package app;
 
 public class Aluno {
-    String nome;
-    int idade;
+    public String nome;
+    public int idade;
+    public String sexo;
+    public int cpf;
+    
+    public String getAbacaxi() {
+        return this.nome + " ( " + this.idade + " anos) "; //(this) Operador de desambiguação    (+) publico
+    }
+    public Aluno(String nome, int idade) {
+        this.nome = nome; 
+        this.idade = idade;
+
+    }
+    public Aluno() {
+
+    }
 }
--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,38 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Que frio está hoje em Pompéia!!!");
+        Aluno a1 = new Aluno();
+        a1.nome = "Gabriel Castão Graciano";
+        a1.idade = 28;
+        a1.sexo = "Masculino";
+        //a1.cpf = 45855689808;
+        
+        System.out.println("[A1]");
+        Aluno a2 = new Aluno();
+        a2.nome = "Guilherme";
+        a2.idade = 33;
+        System.out.println("nome = " + a1.nome);
+        System.out.println("Sexo = " + a1.sexo);
+        //System.out.println("CPF = " + a1.cpf);
+        
+        System.out.println("[A2]");
+        System.out.println("nome = " + a2.nome);
+        System.out.println("idade = " + a2.idade);
+        //System.out.println("idade = " + a1.idade);;
+        //System.out.println("Que frio está hoje em Pompéia!!!");
+
+        System.out.println("[Chamada de método]");
+        System.out.println("A1 = " + a1.getAbacaxi());
+        System.out.println("A2 = " + a2.getAbacaxi());
+
+        Aluno a3 = new Aluno("Maria" , 53);
+        System.out.println("[A3] = "+ a3.getAbacaxi());
+
+        Cliente c1 = new Cliente();
+        c1.setNome("Teste");
+        System.out.println("[CHAMADA DE GET]");
+        System.out.println(c1.getNome());
     }
 }
+//atribuição por cópia 
+//atribuição por espaço de memória
